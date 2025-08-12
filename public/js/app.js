5ff40a30660cc1fcdfd1e256fa907a81ebec56cf
Biniam Kassahun <george.beng@gmail.com>
2025-08-12 12:34:24 +0300

    Add WebRTC functionality and UI elements: include buttons for call control and video elements in index.html, create style.css for background color, and implement WebRTC logic in app.js.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,58 @@
 console.log("Live Communication Platform");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+async function startWebRTC() {
+  const peerConnection = new RTCPeerConnection({
+    iceServers: [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:freestun.net:3478" },
+      { urls: "stun:openrelay.metered.ca:80" },
+      { urls: "stun:stunserver2024.stunprotocol.org:3478" },
+    ],
+    iceTransportPolicy: "all",
+  });
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+
+  stream.getTracks().forEach((track) => {
+    console.log("Track", track);
+    if (track.kind === "video") {
+      localVideo.srcObject = stream;
+    }
+    // pc.addTrack(track, stream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    console.log("ontrack", event);
+    event.streams.forEach((stream) => {
+      remoteStream = stream;
+      stream.getTracks().forEach((track) => {
+        track.onended = () => {
+          console.log("track ended");
+        };
+      });
+    });
+  };
+
+  peerConnection.oniceconnectionstatechange = (event) => {
+    console.log("ICE Connection State Change", event);
+  };
+
+  peerConnection.onicecandidate = (event) => {
+    console.log("ICE Candidate", event);
+  };
+
+  peerConnection.oniceconnectionstatechange = (event) => {
+    console.log("ICE Connection State Change", event);
+  };
+}
+
+startButton.addEventListener("click", async () => {
+  startWebRTC();
+});
