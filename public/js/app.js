f062846d1c572f00a144fd627c02fe149d3ddeb6
Biniam Kassahun <george.beng@gmail.com>
2025-08-12 12:52:20 +0300

    add socket io client for web

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,22 @@ const stopButton = document.getElementById("stopButton");
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
 
-async function startWebRTC() {
-  const peerConnection = new RTCPeerConnection({
-    iceServers: [
-      { urls: "stun:stun.l.google.com:19302" },
-      { urls: "stun:freestun.net:3478" },
-      { urls: "stun:openrelay.metered.ca:80" },
-      { urls: "stun:stunserver2024.stunprotocol.org:3478" },
-    ],
-    iceTransportPolicy: "all",
-  });
+const socket = io("ws://127.0.0.1:3000");
+const peerConnection = new RTCPeerConnection({
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:freestun.net:3478" },
+    { urls: "stun:openrelay.metered.ca:80" },
+    { urls: "stun:stunserver2024.stunprotocol.org:3478" },
+  ],
+  iceTransportPolicy: "all",
+});
 
+socket.on("connect", () => {
+  console.log("Connected to server", socket.id);
+});
+
+async function startWebRTC() {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
